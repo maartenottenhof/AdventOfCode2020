3d8368a711e8d3ce4cfc927fc368f051ad8271d4
Maarten Ottenhof <maarten.ottenhof@jcore.com>
2020-12-05 08:21:31 +0100

    Day 5 solution

---
+++ src/day5/Main.kt
@@ -0,0 +1,17 @@
+package day5
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val passIds = File("src/day5/input.txt").readLines()
+        .map { it.replace("[BR]".toRegex(), "1") }
+        .map { it.replace("[FL]".toRegex(), "0") }
+        .map { it.toInt(2) }
+
+    println(task1(passIds))
+    println(task2(passIds))
+}
+
+private fun task1(passIds: List<Int>) = passIds.max()
+
+private fun task2(passIds: List<Int>) = passIds.min()!!..passIds.max()!! subtract passIds
