d283f5e1d2427e5f7bfba8cddf25418185d846ea
Maarten Ottenhof <maarten.ottenhof@jcore.com>
2020-12-07 12:47:51 +0100

    Updated recursive solution for day 7

--- src/day7/Main.kt
+++ src/day7/Main.kt
@@ -1,57 +1,29 @@
 package day7
 
 import java.io.File
-import java.util.*
 
 fun main() {
-    val bags = File("src/day7/input.txt").readText().removeSuffix(".")
-        .split(".")
-        .filterNot { it.contains("no other") }
-        .map { it.trim().replace("bag[s]?".toRegex(), "").replace("contain", ",").split(",") }
-        .map { it.map(String::trim) }
-        .map { Bag(it[0], toInnerBags(it.subList(1, it.size))) }
-        .map { it.colour to it }.toMap()
+    val bags = File("src/day7/input.txt").readText().split(".").filterNot { it.contains("no other") }
+        .map { it.replace("bag[s]?".toRegex(), "").replace("contain", ",").split(",").map(String::trim) }
+        .map { it[0] to Bag(it[0], toInnerBags(it.subList(1, it.size))) }.toMap()
 
-    task1(bags).let(::println)
-    task2("shiny gold", bags).let(::println)
+    println(task1("shiny gold", bags))
+    println(task2("shiny gold", bags))
 }
 
-private fun task1(bags: Map<String, Bag>) = bags.filter { it ->
-    val queue = LinkedList(it.value.innerBags.keys)
-    while (queue.isNotEmpty()) {
-        if (queue.first == "shiny gold") {
-            return@filter true
-        } else {
-            bags[queue.removeFirst()]?.let { it.innerBags.keys.forEach() { clr -> queue.add(clr) } }
-        }
-    }
-    false
-}.count()
+private fun task1(colour: String, bags: Map<String, Bag>) =
+    bags.filter { it.value.containsColour(colour, bags) }.count()
 
-private fun task2(colour: String, bags: Map<String, Bag>) = bags[colour]?.let {
-    var numberOfBags = 0
-    val queue = LinkedList<String>()
-    it.innerBags.forEach { (colour, amount) ->
-        repeat(amount) {
-            queue.add(colour)
-            numberOfBags++
-        }
-    }
+private fun task2(colour: String, bags: Map<String, Bag>) = bags[colour]?.numberOfInnerBags(bags)
 
-    while (queue.isNotEmpty()) {
-        bags[queue.removeFirst()]?.let { bag ->
-            bag.innerBags.forEach { (colour, amount) ->
-                repeat(amount) {
-                    queue.add(colour)
-                    numberOfBags++
-                }
-            }
-        }
-    }
-    numberOfBags
+private data class Bag(val colour: String, val innerBags: Map<String, Int>) {
+    fun containsColour(colour: String, bags: Map<String, Bag>): Boolean =
+        if (this.innerBags.keys.contains(colour)) true
+        else this.innerBags.keys.any { bags[it]?.containsColour(colour, bags) == true }
+
+    fun numberOfInnerBags(bags: Map<String, Bag>): Int =
+        this.innerBags.entries.sumBy { (bags[it.key]?.numberOfInnerBags(bags) ?: 0) * it.value + it.value }
 }
-
-private data class Bag(val colour: String, val innerBags: Map<String, Int>)
 
 private fun toInnerBags(input: List<String>) =
     input.map { it.substring(2) to Integer.parseInt(it.substring(0, 1)) }.toMap()
