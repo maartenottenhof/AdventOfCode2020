1e49bc22c94d6ac2f3691e2fe44e39c112436a31
Maarten Ottenhof <maarten.ottenhof@jcore.com>
2020-12-13 18:50:04 +0100

    Solution for day 13-2

--- src/day13/Main.kt
+++ src/day13/Main.kt
@@ -5,14 +5,25 @@ import java.io.File
 fun main() {
     val input = File("src/day13/input.txt").readLines()
     val timestamp = input[0].toInt()
-    val busses = input[1].split(",")
+    val buses = input[1].split(",")
 
-    println(task1(busses, timestamp))
+    println(task1(buses, timestamp))
+    println(task2(buses))
 }
 
-private fun task1(busses: List<String>, timestamp: Int) = busses.filterNot { it == "x" }
-    .map { it.toInt() }
-    .map { i -> i to generateSequence(i) { it + i }.first { it >= timestamp } }
-    .minBy { it.second }.let { (it!!.second - timestamp) * it.first }
+private fun task1(buses: List<String>, timestamp: Int) =
+    buses.filterNot { it == "x" }.map { it.toInt() }
+        .map { i -> i to generateSequence(i) { it + i }.first { it >= timestamp } }
+        .minBy { it.second }.let { (it!!.second - timestamp) * it.first }
 
+private fun task2(buses: List<String>) =
+    buses.mapIndexed { index, s -> s to index }
+        .filterNot { it.first == "x" }
+        .map { it.first.toLong() to it.second.toLong() }.let { findFirstBus(it) }
 
+private fun findFirstBus(input: List<Pair<Long, Long>>) =
+    input.fold(1L) { acc, pair -> acc * pair.first }.let { p ->
+        input.map { (bus, index) ->
+            index * (p / bus).toBigInteger().modInverse(bus.toBigInteger()).toLong() * (p / bus)
+        }.sum().let { p - it % p }
+    }
