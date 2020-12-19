0a07ea5f014caa0565e1a8eb8e101fc39da28b9d
Maarten Ottenhof <maarten.ottenhof@jcore.com>
2020-12-19 13:55:00 +0100

    Refactored solution for day 18

--- src/day18/Main.kt
+++ src/day18/Main.kt
@@ -3,47 +3,43 @@ package day18
 import java.io.File
 
 fun main() {
-    val input = File("src/day18/input.txt").readLines()
+    val sums = File("src/day18/input.txt").readLines()
         .map { it.replace("(", " ( ").replace(")", " ) ").trim().split("\\s+".toRegex()) }
 
-    input.map { it.solveParentheses() }.sum().let { println(it) }
+    println(task1(sums))
+    println(task2(sums))
 }
 
-private fun List<String>.solveParentheses(): Long {
-    if (!contains("(")) {
-        return solveMaths2()
-    }
+private fun task1(sums: List<List<String>>) = sums.sumOf { it.solveParentheses(List<String>::solveEqualPrecedence) }
+private fun task2(sums: List<List<String>>) = sums.sumOf { it.solveParentheses(List<String>::solvePlusFirst) }
+
+private fun List<String>.solveParentheses(solver: (List<String>) -> (Long)): Long {
+    if (!contains("(")) return solver(this)
 
     val lastOpenIndex = indexOfLast { it == "(" }
     val matchingCloseIndex = drop(lastOpenIndex).indexOfFirst { it == ")" } + lastOpenIndex
-    val solvedBrackets = subList(lastOpenIndex + 1, matchingCloseIndex).solveMaths2()
-    val newList = subList(0, lastOpenIndex).plus(solvedBrackets.toString()).plus(subList(matchingCloseIndex + 1, size))
+    val newList = subList(0, lastOpenIndex)
+        .plus(solver.invoke(subList(lastOpenIndex + 1, matchingCloseIndex)).toString())
+        .plus(subList(matchingCloseIndex + 1, size))
 
-    return if (newList.contains("(")) {
-        newList.solveParentheses()
-    } else {
-        newList.solveMaths2()
-    }
+    return if (newList.contains("(")) newList.solveParentheses(solver)
+    else solver.invoke(newList)
 }
 
-private fun List<String>.solveMaths() : Long {
+private fun List<String>.solveEqualPrecedence() : Long {
     val numberOne = this[0].toLong()
-    val operator = this[1]
     val numberTwo = this[2].toLong()
-    val newValue = when (operator) {
+    val newValue = when (this[1]) {
         "+" -> numberOne + numberTwo
         "*" -> numberOne * numberTwo
-        else -> error("Unexpected operator: $operator")
+        else -> error("Unexpected operator: $this[1]")
     }
 
-    return if (size == 3) {
-        newValue
-    } else {
-        (listOf(newValue.toString()) + this.subList(3, size)).solveMaths2()
-    }
+    return if (size == 3) newValue
+    else (listOf(newValue.toString()) + this.subList(3, size)).solveEqualPrecedence()
 }
 
-private fun List<String>.solveMaths2() : Long {
+private fun List<String>.solvePlusFirst() : Long {
     val newList: List<String> = if (contains("+")) {
         val plusIndex = indexOfFirst { it == "+" }
         subList(0, plusIndex - 1)
@@ -53,9 +49,6 @@ private fun List<String>.solveMaths2() : Long {
         (listOf((this[0].toLong() * this[2].toLong()).toString()) + this.subList(3, size))
     }
 
-    return if (size == 3) {
-        newList[0].toLong()
-    } else {
-        newList.solveMaths2()
-    }
+    return if (size == 3) newList[0].toLong()
+    else newList.solvePlusFirst()
 }
