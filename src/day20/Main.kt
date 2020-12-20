68bdae4caf0c3610ee66ba39cdce0b2f4c3f545a
Maarten Ottenhof <maarten.ottenhof@jcore.com>
2020-12-20 11:02:16 +0100

    Cleaned up day 20-1

--- src/day20/Main.kt
+++ src/day20/Main.kt
@@ -1,40 +1,65 @@
 package day20
 
 import java.io.File
+import java.util.*
 
 fun main() {
-    val input = File("src/day20/input.txt").readLines()
-    val tiles = mutableListOf<Tile>()
+    val tiles = File("src/day20/input.txt").readLines()
+        .toPossibleTiles()
+        .toMatchingTiles()
 
-    for (i in 0..input.size step 12) {
+    println(task1(tiles))
+}
+
+private fun task1(definitiveTiles: MutableList<Tile>) =
+    definitiveTiles.mapNotNull { tile ->
+        definitiveTiles.filter { tile.match(it) }.size
+            .let { if (it == 2) tile.id else null }
+    }.map { it.toLong() }.fold(1L, Long::times)
+
+private fun List<Tile>.toMatchingTiles(): MutableList<Tile> {
+    val definitiveTiles = mutableListOf(first())
+    val tilesToResolve = LinkedList(mutableListOf(first()))
+
+    while (tilesToResolve.isNotEmpty()) {
+        val tile = tilesToResolve.pop()
+        filter { tile.match(it) }
+            .filter { definitiveTiles.none { i -> i.id == it.id } }
+            .forEach {
+                tilesToResolve.add(it)
+                definitiveTiles.add(it)
+            }
+    }
+
+    return definitiveTiles
+}
+
+private fun List<String>.toPossibleTiles(): List<Tile> {
+    val tiles = mutableListOf<Tile>()
+    for (i in 0..size step 12) {
         tiles.add(
             Tile(
-                input[i].replace("Tile ", "").replace(":", ""),
-                input[i + 1],
-                input.subList(i + 1, i + 11).map { it.last() }.joinToString(""),
-                input[i + 10],
-                input.subList(i + 1, i + 11).map { it.first() }.joinToString("")
+                this[i].replace("Tile ", "").replace(":", ""),
+                this[i + 1],
+                subList(i + 1, i + 11).map { it.last() }.joinToString(""),
+                this[i + 10],
+                subList(i + 1, i + 11).map { it.first() }.joinToString("")
             )
         )
     }
 
-    val possibleTiles = tiles.flatMap { tile ->
+    return tiles.flatMap { tile ->
         val rotated = (0 until 3).fold(listOf(tile)) { acc, _ -> acc + acc.last().rotateRight() }
         val flipRotated = (0 until 3).fold(listOf(tile.flip())) { acc, _ -> acc + acc.last().rotateRight() }
         rotated + flipRotated
-    }.toMutableList()
-
-    tiles.mapNotNull { tile ->
-        possibleTiles.filter {
-            tile.up == it.down && tile.id != it.id ||
-                    tile.left == it.right && tile.id != it.id ||
-                    tile.right == it.left && tile.id != it.id ||
-                    tile.down == it.up && tile.id != it.id
-        }.size.let { if (it == 2) tile.id else null }
-    }.map { it.toLong() }.fold(1L, Long::times).let { println(it) }
+    }
 }
 
 data class Tile(val id: String, val up: String, val right: String, val down: String, val left: String) {
     fun flip() = Tile(id, up.reversed(), left, down.reversed(), right)
     fun rotateRight() = Tile(id, left.reversed(), up, right.reversed(), down)
+    fun match(tile: Tile) = tile.up == down && tile.id != id ||
+            tile.left == right && tile.id != id ||
+            tile.right == left && tile.id != id ||
+            tile.down == up && tile.id != id
 }
