192837a257748eaed684dfab58ebcf6fd0402480
Maarten Ottenhof <maarten.ottenhof@jcore.com>
2020-12-27 13:46:53 +0100

    Day 22

--- src/day22/Main.kt
+++ src/day22/Main.kt
@@ -8,7 +8,7 @@ fun main() {
     val deck1 = input.drop(1).takeWhile { it.isNotBlank() }.map { it.toInt() }
     val deck2 = input.drop(1).dropWhile { !it.contains("Player 2:") }.drop(1).map { it.toInt() }
 
-//    println(task1(LinkedList(deck1), LinkedList(deck2)))
+    println(task1(LinkedList(deck1), LinkedList(deck2)))
     println(task2(LinkedList(deck1), LinkedList(deck2)))
 }
 
@@ -17,21 +17,21 @@ private fun task1(deck1: LinkedList<Int>, deck2: LinkedList<Int>): Int {
         val card1 = deck1.pop()
         val card2 = deck2.pop()
 
-        if (card1 > card2) {
-            deck1.add(card1)
-            deck1.add(card2)
-        } else {
-            deck2.add(card2)
-            deck2.add(card1)
-        }
+        if (card1 > card2) deck1.add(card1, card2)
+        else deck2.add(card2, card1)
     }
 
-    return (deck1 + deck2).reversed().mapIndexed { index, i -> (index + 1) * i }.sum()
+    return (deck1 + deck2).reversed()
+        .mapIndexed { index, i -> (index + 1) * i }
+        .sum()
 }
 
 private fun task2(deck1: LinkedList<Int>, deck2: LinkedList<Int>): Int {
-    return recursiveCombat(deck1, deck2).let { it.first + it.second }
-        .reversed().mapIndexed { index, i -> (index + 1) * i }.sum()
+    return recursiveCombat(deck1, deck2)
+        .let { it.first + it.second }
+        .reversed()
+        .mapIndexed { index, i -> (index + 1) * i }
+        .sum()
 }
 
 private fun recursiveCombat(deck1: LinkedList<Int>, deck2: LinkedList<Int>): Pair<LinkedList<Int>, LinkedList<Int>> {
@@ -47,23 +47,16 @@ private fun recursiveCombat(deck1: LinkedList<Int>, deck2: LinkedList<Int>): Pai
         val card2 = deck2.pop()
 
         if ((card1 <= deck1.size && card2 <= deck2.size)) {
-            val result = recursiveCombat(
-                LinkedList(deck1.subList(0, card1)),
-                LinkedList(deck2.subList(0, card2))
-            )
+            val result = recursiveCombat(LinkedList(deck1.subList(0, card1)), LinkedList(deck2.subList(0, card2)))
             if (result.first.isNotEmpty()) {
-                deck1.add(card1)
-                deck1.add(card2)
+                deck1.add(card1, card2)
             } else {
-                deck2.add(card2)
-                deck2.add(card1)
+                deck2.add(card2, card1)
             }
         } else if (card1 > card2) {
-            deck1.add(card1)
-            deck1.add(card2)
+            deck1.add(card1, card2)
         } else {
-            deck2.add(card2)
-            deck2.add(card1)
+            deck2.add(card2, card1)
         }
     }
 
